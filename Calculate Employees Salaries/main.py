de35b9b25c5b2bde22d03d9054884d4b304123b1
DREAMToBeSucessfulPerson <52617968+DREAMToBeSucessfulPerson@users.noreply.github.com>
2019-07-11 13:06:20 +0530

    Add files via upload
    Note that sometimes the script need google apis to work fine.

---
+++ Calculate Employees Salaries/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+employees = pd.read_csv(r"EMPLOYEES.csv")
+departments = pd.read_csv(r"DEPARTMENTS.csv")
+job_history = pd.read_csv(r"JOB_HISTORY.csv")
+jobs = pd.read_csv(r"JOBS.csv")
+countries = pd.read_csv(r"COUNTRIES.csv")
+regions = pd.read_csv(r"REGIONS.csv")
+locations = pd.read_csv(r"LOCATIONS.csv")
+
+result = job_history.groupby(['employee_id']) 
+
+print(employees.agg({'salary': ['min', 'max', 'mean', 'median']}))
